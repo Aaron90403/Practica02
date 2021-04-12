f6968e4ead58773643973a364f6e3d2b9f9695ae
Aaron Josué Josué <>
2021-04-12 18:40:30 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,8 @@
+
+var fecha = "12 del 4 del 2021"
+let dia = "lunes"
+let profe = "Luis Carlos Garcia"
+let nombre = "Aaron Montoya Diaz"
+print("Hola profe",profe)
+print("Hoy es",fecha)
+print("Hoy es",dia)
